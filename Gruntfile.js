9d72f6a9703c284c103fe7d67f23f4f7198bc682
Marcos Pereira <marcospereira.mpj@gmail.com>
2017-02-04 18:48:53 -0300

    Configured grunt file.

---
+++ Gruntfile.js
@@ -0,0 +1,219 @@
+'use strict';
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Show grunt task time
+    require('time-grunt')(grunt);
+
+    // Configurable paths for the app
+    var appConfig = {
+        app: 'app',
+        dist: 'dist'
+    };
+
+    // Grunt configuration
+    grunt.initConfig({
+
+        // Project settings
+        m2agro: appConfig,
+
+        // The grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: false,
+                    middleware: function (connect) {
+                        return [
+                            connect.static('.tmp'),
+                            connect().use(
+                                '/bower_components',
+                                connect.static('./bower_components')
+                            ),
+                            connect.static(appConfig.app)
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    open: true,
+                    base: '<%= m2agro.dist %>'
+                }
+            }
+        },
+        // Compile less to css
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    optimization: 2
+                },
+                files: {
+                    "app/assets/styles/styles.css": "app/assets/less/styles.less"
+                }
+            }
+        },
+        // Watch for changes in live edit
+        watch: {
+            styles: {
+                files: ['app/assets/less/**/*.less'],
+                tasks: ['less', 'copy:styles'],
+                options: {
+                    nospawn: true,
+                    livereload: '<%= connect.options.livereload %>'
+                },
+            },
+            js: {
+                files: ['<%= m2agro.app %>/scripts/{,*/}*.js'],
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= m2agro.app %>/**/*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    '<%= m2agro.app %>/app/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        // If you want to turn on uglify you will need write your angular code with string-injection based syntax
+        // For example this is normal syntax: function exampleCtrl ($scope, $rootScope, $location, $http){}
+        // And string-injection based syntax is: ['$scope', '$rootScope', '$location', '$http', function exampleCtrl ($scope, $rootScope, $location, $http){}]
+        uglify: {
+            options: {
+                mangle: false
+            }
+        },
+        // Clean dist folder
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= m2agro.dist %>/{,*/}*',
+                        '!<%= m2agro.dist %>/.git*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+        // Copies remaining files to places other tasks can use
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: '<%= m2agro.app %>',
+                        dest: '<%= m2agro.dist %>',
+                        src: [
+                            '*.{ico,png,txt}',
+                            '.htaccess',
+                            '*.html',
+                            'views/{,*/}*.html',
+                            'styles/patterns/*.*',
+                            'img/{,*/}*.*'
+                        ]
+                    },
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/fontawesome',
+                        src: ['fonts/*.*'],
+                        dest: '<%= m2agro.dist %>'
+                    },
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: 'bower_components/bootstrap',
+                        src: ['fonts/*.*'],
+                        dest: '<%= m2agro.dist %>'
+                    },
+                ]
+            },
+            styles: {
+                expand: true,
+                cwd: '<%= m2agro.app %>/assets/styles',
+                dest: '.tmp/styles/',
+                src: '{,*/}*.css'
+            }
+        },
+        // Renames files for browser caching purposes
+        filerev: {
+            dist: {
+                src: [
+                    '<%= m2agro.dist %>/scripts/{,*/}*.js',
+                    '<%= m2agro.dist %>/styles/{,*/}*.css',
+                    '<%= m2agro.dist %>/assets/styles/fonts/*'
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true,
+                    conservativeCollapse: true,
+                    collapseBooleanAttributes: true,
+                    removeCommentsFromCDATA: true,
+                    removeOptionalTags: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= m2agro.dist %>',
+                    src: ['*.html', 'views/{,*/}*.html'],
+                    dest: '<%= m2agro.dist %>'
+                }]
+            }
+        },
+        useminPrepare: {
+            html: 'app/index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+        usemin: {
+            html: ['dist/index.html']
+        }
+    });
+
+    // Run live version of app
+    grunt.registerTask('live', [
+        'clean:server',
+        'copy:styles',
+        'connect:livereload',
+        'watch'
+    ]);
+
+    // Run build version of app
+    grunt.registerTask('server', [
+        'build',
+        'connect:dist:keepalive'
+    ]);
+
+    // Build version for production
+    grunt.registerTask('build', [
+        'clean:dist',
+        'less',
+        'useminPrepare',
+        'concat',
+        'copy:dist',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
+
+};
